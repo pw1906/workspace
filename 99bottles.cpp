1bec787e4675ac232375b480d3fedde07ee3b62d
pw1906 <81563881+pw1906@users.noreply.github.com>
2021-04-07 17:23:26 +0200

    Add files via upload

---
+++ 99bottles.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    for (int i = 99; i > 0; i--){
+
+        std::cout << i << "bottles of pop on the wall\n";
+        std::cout << "Take one down and pass it around.\n";
+        std::cout << i - 1 << "bottles of pop on the wall.\n\n";
+        
+    }
+}
